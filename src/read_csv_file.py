6216f129b86895dede955528654fb5d78f90d8c2
rajar <rajarajeshwarir2021@gmail.com>
2025-02-13 18:30:53 +0100

    feature(app): implement test to read_csv_file and throws exception when file doesn't exist

--- src/read_csv_file.py
+++ src/read_csv_file.py
@@ -1,5 +1,9 @@
+import os
 import pandas as pd
 
 
 def read_csv_file(filepath: str):
+    if not os.path.exists(filepath):
+        raise Exception("File does not exist")
     return pd.read_csv(filepath)
+
--- test/test_read_csv_file.py
+++ test/test_read_csv_file.py
@@ -1,10 +1,14 @@
+import os
+
 import pandas as pd
+import pytest
 
 from src.read_csv_file import read_csv_file
 from unittest.mock import MagicMock
 
 
-def test_read_csv_file_return_correct_dataframe(monkeypatch):
+@pytest.fixture()
+def mock_read_csv(monkeypatch):
     # Create a mock DataFrame to return
     mock_df = pd.DataFrame({
         "Name": ["Alice", "Bob"],
@@ -17,6 +21,13 @@ def test_read_csv_file_return_correct_dataframe(monkeypatch):
 
     # Use monkeypatch to replace the pandas.read_csv with the mock
     monkeypatch.setattr(pd, "read_csv", mock_read_csv)
+    return mock_read_csv
+
+
+def test_read_csv_file_return_correct_dataframe(mock_read_csv, monkeypatch):
+    # Mock os.path.exists to return False (simulating the file doesn't exist)
+    mock_exists = MagicMock(return_value=True)
+    monkeypatch.setattr(os.path, "exists", mock_exists)
 
     # Call the function under test
     result = read_csv_file("sample_file.csv")
@@ -25,4 +36,15 @@ def test_read_csv_file_return_correct_dataframe(monkeypatch):
     mock_read_csv.assert_called_once_with("sample_file.csv")
 
     # Assert that the result is the DataFrame we mocked
-    pd.testing.assert_frame_equal(result, mock_df)
+    pd.testing.assert_frame_equal(result, mock_read_csv.return_value)
+
+
+def test_read_csv_file_throw_exception_file_not_exist(monkeypatch):
+    # Mock os.path.exists to return False (simulating the file doesn't exist)
+    mock_exists = MagicMock(return_value=False)
+    monkeypatch.setattr(os.path, "exists", mock_exists)
+
+    # Use pytest.raises to check if the exception is raised
+    with pytest.raises(Exception, match="File does not exist"):
+        read_csv_file("sample.csv")
+
